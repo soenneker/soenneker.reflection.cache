ff72b517e6b7cb37c0dda350afb63ab8bfb5958b
Jake Soenneker <jake@soenneker.com>
2024-02-09 14:48:54 -0600

    Additional field and property info extensions for constant checking

---
+++ src/Extensions/FieldInfoExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.Contracts;
+using System.Reflection;
+
+namespace Soenneker.Reflection.Cache.Extensions;
+
+public static class FieldInfoExtension
+{
+    [Pure]
+    public static bool IsConstant(this FieldInfo field)
+    {
+        return field.IsLiteral && !field.IsInitOnly;
+    }
+}
---
+++ src/Extensions/PropertyInfoExtension.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.Contracts;
+using System.Reflection;
+
+namespace Soenneker.Reflection.Cache.Extensions;
+
+public static class PropertyInfoExtension
+{
+    // TODO: Move to CachedProperty
+    [Pure]
+    public static bool IsConstant(this PropertyInfo property)
+    {
+        MethodInfo? getMethod = property.GetMethod;
+
+        if (getMethod == null)
+        {
+            // If there's no getter, it's not a constant property
+            return false;
+        }
+
+        if (!getMethod.IsStatic)
+        {
+            return false;
+        }
+
+        // Check if the property is readonly
+        if (!getMethod.IsPublic || property.SetMethod != null)
+        {
+            return false;
+        }
+
+        // If the property meets the above conditions, it's likely a constant
+        return true;
+    }
+}
